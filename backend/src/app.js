34c618ec59a17b4ed9b56067b77efe897b4c0ee2
[Pranav Fataniya] <pranavfataniya2005@gmail.com>
2025-08-15 21:17:52 +0530

    first commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,8 @@ const app = express();
 // ORIGIN FROM WHERE WE ACCEPT OUR REQUEST
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://localhost:5174"
+  "http://localhost:5174",
+  "https://e-book-ten.vercel.app/"
 ];
 
 app.use(cors({
